2a928352d530f35280891d6d56997dfcb783e404
Marvin-Sy <janmartinvincent@gmail.com>
2022-01-28 21:48:23 +0800

    Added comments on the process of logging data for ML.

--- src/main/java/prmts/org/parser/model/RowData.java
+++ src/main/java/prmts/org/parser/model/RowData.java
@@ -5,13 +5,43 @@ import com.opencsv.bean.CsvBindByPosition;
 
 public class RowData {
     @CsvBindByPosition(position = 0)
-    private String seconds;
+    private String seconds; // seconds need to be int or at least without the 's' at the end.
     @CsvBindByPosition(position = 1)
     private int x_origin;
     @CsvBindByPosition(position = 2)
     private int y_origin;
     @CsvBindByPosition(position = 3)
     private int floor;
+
+    /*
+    * x_origin, y_origin, and floor should never change beyond the first read.
+    *
+    * Additional needed variables:
+    * - int x_start
+    * - int y_start
+    * - int floor_start
+    * - int x_end
+    * - int y_end
+    * - int floor_end
+    *
+    *  In order to log all start and end variables, you need to have the previous row data except on the first read.
+    *
+    *  On first read:
+    *    x_start = x_origin
+    *    y_start = y_origin
+    *    floor_start = floor_origin
+    *    x_end = x_origin + width
+    *    y_end = y_origin + depth
+    *    floor_end = floor_origin
+    *
+    *  On second read and beyond:
+    *    x_start = { newX_origin < prevX_start ? newX_origin : prevX_start }
+    *    y_start = { newY_origin < prevY_start ? newY_origin : prevY_start }
+    *    floor_start = { newFloor_origin < prevFloor_start ? newFloor_origin : prevFloor_start }
+    *    x_end = { newX_origin + width > prevX_end ? newX_origin + width : prevX_end }
+    *    y_end = { newY_origin + depth > prevY_end ? newY_origin + depth : prevY_end }
+    *    floor_end = { newFloor_origin > prevFloor_end ? newFloor_origin : prevFloor_end }
+    * */
 
     public RowData(AlarmLog log, Compartment compartment) {
         this.seconds = log.getSeconds();
